527662d2a2c6c86126850a437d4d740f120a76ef
rashed-ahammod <ahammodr74@gmail.com>
2025-11-03 12:20:24 +0600

    1st task

--- Mid/Lab Task 1/task.php
+++ Mid/Lab Task 1/task.php
@@ -1,1 +1,73 @@
-#1st task
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AIUB Registration System</title>
+</head>
+<body>
+    <style>
+        body{
+            background-color:lightblue;
+        }
+        h1{
+            color:red;
+        }
+
+.frame{
+        width: 500px;
+        margin: auto;
+        padding:10px;
+        background-color:white;
+
+    }
+    
+</style>
+<div class="frame">
+<h1 allign="left" style ="color:red;border-bottom:3px solid red">Student Registration Form</h1>
+
+<table>
+First Name<br>
+<input type="text"><br><br>
+Last Name<br>
+<input type="text"><br><br>
+Student ID<br>
+<input type="num"><br><br>
+Program/Major<br>
+<input type="dropdown">
+<br><br>
+Phone<br>
+<input type="num"><br><br>
+Uni mail<br>
+<input type="text"><br><br>
+Create Password<br>
+<input type="text"><br><br>
+Confirm Password<br>
+<input type="text"><br><br>
+
+
+
+Semester Selection<br>
+<input type="checkbox">Summer 2024
+<input type="checkbox">Fall 2024
+<input type="checkbox">Spring 2025
+<input type="checkbox">other/Special Team
+<br>
+
+<br>
+Requested Credit Laod(Max 15)<br>
+<input type="text"><br>
+<input type="checkbox">I requie academic advising before final registration
+<br><br>
+<h1 allign="left" style ="color:red;border-bottom:3px solid red">Course Selection</h1><br>
+<input type="checkbox">MATH1201(Calculus)
+<input type="checkbox">CS 2105(Data Structure)
+<input type="checkbox">ECON 1001(Microeconomic)
+<input type="checkbox">PHY(Physics Lab)<br><br>
+Comments/Special Request<br>
+<input type="text">
+<br><br>
+<center><input type="submit"></center>
+
+</div>
+</body>
+</html>
