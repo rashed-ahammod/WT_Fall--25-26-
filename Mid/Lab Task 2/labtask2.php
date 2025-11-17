5a9a677d62e7eacb5cfe7191d320366ebdfb8728
rashed-ahammod <ahammodr74@gmail.com>
2025-11-17 11:02:46 +0600

    body css

--- Mid/Lab Task 2/labtask2.php
+++ Mid/Lab Task 2/labtask2.php
@@ -1,1 +1,15 @@
-//even task
+
+<!doctype html>
+<html>
+    <head>
+        <title>Registration Form</title>
+        <style>
+            body{
+                font-family:Arial,sans-serif;
+                padding:30px;
+                background-color:#bfe0da;
+            }
+
+        </style>
+    </head>
+</html>
