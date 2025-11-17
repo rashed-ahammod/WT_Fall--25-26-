0ca1a8221933c217a72f25b5ae186d491d8dc5e0
rashed-ahammod <ahammodr74@gmail.com>
2025-11-17 11:06:15 +0600

    form css

--- Mid/Lab Task 2/labtask2.php
+++ Mid/Lab Task 2/labtask2.php
@@ -9,6 +9,15 @@
                 padding:30px;
                 background-color:#bfe0da;
             }
+            form{
+                background-color: #ffffff;
+                padding: 40px;
+                border-radius: 10px;
+                width: 300px;
+                margin: 0 auto;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            }
+            
 
         </style>
     </head>
