2253a9161ffd75182cbc77726392a707efb2f51d
rashed-ahammod <ahammodr74@gmail.com>
2025-12-15 12:10:24 +0600

    php1st

--- Final/Validation Problem/validation.php
+++ Final/Validation Problem/validation.php
@@ -2,8 +2,26 @@
 
 <html>
 
+<?php
+$name="";
+$email="";
+$nameerror="";
+$emailerror="";
+if(empty($_POST["name"]||$_POST["emall"]))
+{
+    $nameerror="fillup all";
+}
+else if()
+
+?>
+
 <form>
 Name:<input type="text" name="name">
 Email:<input type="text" name="email">
 <input type="submit" name="Submit"value="Submit">
 </form>
+
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"&& empty($nameerror)){
+    echo"<h3>Name:.$name";
+}
