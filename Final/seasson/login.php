a0e24b262b4d84854216406dab0925d845e82a63
rashed-ahammod <ahammodr74@gmail.com>
2026-01-07 02:23:00 +0600

    ss1

--- Final/seasson/login.php
+++ Final/seasson/login.php
@@ -13,4 +13,19 @@ Password : <input type="text" name="password">
 <button type="submit">Login<button>
 </form>
     </body>
-</html>
+    </html>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = test_input($_POST["name"]);
+    $password = test_input($_POST["password"]);
+
+    // Validation
+    if (empty($name) || empty($password)) {
+       
+        echo "Invalid username or password";
+    }else{
+        header("location: loginController.php");
+    }
+}
+?>
--- Final/seasson/loginController.php
+++ Final/seasson/loginController.php
@@ -5,21 +5,14 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $name = test_input($_POST["name"]);
-    $password = test_input($_POST["password"]);
 
-    // Validation
-    if (empty($name) || empty($password)) {
-        echo "Invalid username or password";
-    } else {
-        // Store in session
         $_SESSION["username"] = $name;
         $_SESSION["password"] = $password;
 
         header("Location: welcome.php");
         exit();
     }
-}
+
 
 function test_input($data) {
     return trim($data);
